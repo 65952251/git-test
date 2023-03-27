5ff742d6f573921c7fa33b2e81c04adb64414058
yangj <ffyangjie@chinatelecom.cn>
2023-03-27 19:50:43 +0800

    dev第一次提交

--- src/main/java/com/ffcs/gittest/git/GitTest.java
+++ src/main/java/com/ffcs/gittest/git/GitTest.java
@@ -7,5 +7,6 @@ package com.ffcs.gittest.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("master第一次提交");
+        System.out.println("dev第一次提交");
     }
 }
